6d07484442278d6b619f311c0080233408c52481
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2023-07-03 08:45:22 +0200

    Update AdLibPiece endpoints

--- src/app/rundown/services/ad-lib-piece.service.ts
+++ src/app/rundown/services/ad-lib-piece.service.ts
@@ -4,7 +4,7 @@ import { catchError, Observable } from 'rxjs'
 import { Identifier } from '../../shared/model/identifier'
 import { HttpErrorService } from '../../shared/service/http-error.service'
 
-const AD_LIB_PIECE_URL: string = 'http://localhost:3005/api/rundowns'
+const AD_LIB_PIECE_URL: string = 'http://localhost:3005/api/adLibPieces'
 
 @Injectable()
 export class AdLibPieceService {
@@ -12,14 +12,14 @@ export class AdLibPieceService {
   constructor(private http: HttpClient, private httpErrorService: HttpErrorService) { }
 
   public fetchAdLibPieceIdentifiers(rundownId: string): Observable<Identifier[]> {
-    return this.http.get<Identifier[]>(`${AD_LIB_PIECE_URL}/${rundownId}/adLibPieces`)
+    return this.http.get<Identifier[]>(`${AD_LIB_PIECE_URL}/rundowns/${rundownId}`)
       .pipe(
         catchError((error) => this.httpErrorService.catchError(error))
       )
   }
 
   public executeAdLibPiece(rundownId: string, adLibIdentifier: Identifier): Observable<void> {
-    return this.http.put<void>(`${AD_LIB_PIECE_URL}/${rundownId}/adLibPieces/${adLibIdentifier.id}`, null)
+    return this.http.put<void>(`${AD_LIB_PIECE_URL}/${adLibIdentifier.id}/rundowns/${rundownId}`, null)
       .pipe(
         catchError((error) => this.httpErrorService.catchError(error))
       )
