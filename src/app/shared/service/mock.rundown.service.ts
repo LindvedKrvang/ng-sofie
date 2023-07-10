421067d89d134f2714ecd9ec9208281056720018
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2023-07-10 20:40:37 +0200

    Randomly generate mock Rundown data

--- src/app/shared/service/mock.rundown.service.ts
+++ src/app/shared/service/mock.rundown.service.ts
@@ -4,23 +4,63 @@ import {Piece} from '../model/piece';
 import {RundownService} from './rundown.service';
 import {Observable, of} from 'rxjs';
 import {Identifier} from '../model/identifier';
+import {Segment} from '../model/segment';
+import {Part} from '../model/part';
 
 const RUNDOWNS: Rundown[] = [
-  new Rundown({
-    id: 'rundownOne',
-    name: 'RundownOne',
-    segments: [],
-    isActive: false,
-    infinitePieces: []
-  }),
-  new Rundown({
-    id: 'rundownTwo',
-    name: 'RundownTwo',
-    segments: [],
+  createRundown('R-1'),
+  createRundown('R-2'),
+]
+
+function createRundown(rundownId: string): Rundown {
+  const segments: Segment[] = []
+  for (let i = 0; i < Math.random() * 10; i++) {
+    segments.push(createSegment(`${rundownId} S-${i}`, rundownId))
+  }
+  return new Rundown({
+    id: rundownId,
+    name: `Rundown ${rundownId}`,
+    segments,
     isActive: false,
     infinitePieces: []
   })
-]
+}
+
+function createSegment(segmentId: string, rundownId: string): Segment {
+  const parts: Part[] = []
+  for (let i = 0; i < Math.random() * 10; i++) {
+    parts.push(createPart(`${segmentId} P-${i}`, segmentId))
+  }
+  return new Segment({
+    id: segmentId,
+    rundownId,
+    name: `Segment ${segmentId}`,
+    isNext: false,
+    isOnAir: false,
+    parts
+  })
+}
+
+function createPart(partId: string, segmentId: string): Part {
+  const pieces: Piece[] = []
+  for (let i = 0; i < Math.random() * 2; i++) {
+    pieces.push(
+      {
+        id: `${partId} pi-${i}`,
+        partId,
+        name: `Pi ${i}`,
+        layer: 'someLayer'
+      }
+    )
+  }
+  return new Part({
+    id: partId,
+    segmentId,
+    isOnAir: false,
+    isNext: false,
+    pieces
+  })
+}
 
 @Injectable()
 export class MockRundownService extends RundownService {
